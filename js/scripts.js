e351f948897d8e189405a3acc9ad798b2178bb43
Chris Murone <chris.murone@ge.com>
2017-05-09 12:12:21 -0400

    Meet requirements 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
-var clicks = 0;
-var elem = document.getElementById('cat');
-var displayClicks = document.getElementById('numclicks');
-elem.addEventListener('click', function(){
-  ++ clicks;
-  displayClicks.innerHTML = clicks;
-}, false);
+var content = document.getElementById('content');
+var cats = [
+	{
+		"name":"Jackson",
+		"clicks": 0
+	},
+	{
+		"name":"Ella",
+		"clicks": 0
+	}
+];
+
+
+
+window.onload = function(){
+	console.log(cats);
+	cats.forEach(function(cat){
+		console.log(cat.name);
+		var thiscat = document.createElement("div");
+		thiscat.id = cat.name +'-div';
+		thiscat.className = 'cat-div';
+		thiscat.innerHTML = '<img id="'+ cat.name +'-pic" class="cat-pic" src="img/'+ cat.name +'.jpg">'+
+			'<h2 id="'+ cat.name +'-click-header" class="click-header">'+ cat.name +' Clicks: </h2>'+
+			'<h2 id="'+ cat.name +'-numclicks" class="num-header">'+ cat.clicks +'</h2>';
+		content.appendChild(thiscat);
+		var elem = document.getElementById(cat.name +'-pic');
+		elem.addEventListener('click', function(){
+			++ cat.clicks;
+			document.getElementById(cat.name +'-numclicks').innerHTML = cat.clicks;
+		}, false);
+	});
+};
+
